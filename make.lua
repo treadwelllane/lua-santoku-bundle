82148a8bb6bb69af03eb93147aa40accc9728f3b
broma0 <121064585+broma0@users.noreply.github.com>
2024-02-07 23:59:21 +0000

    bump version to 0.0.26-1

--- make.lua
+++ make.lua
@@ -1,16 +1,16 @@
 local env = {
 
   name = "santoku-bundle",
-  version = "0.0.25-1",
+  version = "0.0.26-1",
   variable_prefix = "TK_BUNDLE",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.179-1",
-    "santoku-system >= 0.0.17-1",
-    "santoku-fs >= 0.0.23-1"
+    "santoku >= 0.0.184-1",
+    "santoku-system >= 0.0.20-1",
+    "santoku-fs >= 0.0.25-1"
   },
 
   test = {
