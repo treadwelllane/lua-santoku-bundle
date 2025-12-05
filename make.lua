af956c62dbcf85e60be4596647b42c9eaa51967c
broma0 <121064585+broma0@users.noreply.github.com>
2025-12-05 18:17:22 -0500

    clean up make.lua formatting

--- make.lua
+++ make.lua
@@ -1,26 +1,17 @@
 local env = {
-
   name = "santoku-bundle",
-  version = "0.0.36-1",
+  version = "0.0.37-1",
   variable_prefix = "TK_BUNDLE",
   license = "MIT",
   public = true,
-
   dependencies = {
     "lua >= 5.1",
     "lpeg >= 1.0.0",
-    "santoku >= 0.0.297-1",
-    "santoku-system >= 0.0.56-1",
-    "santoku-fs >= 0.0.34-1",
+    "santoku >= 0.0.303-1",
+    "santoku-system >= 0.0.58-1",
+    "santoku-fs >= 0.0.37-1",
     "santoku-mustache >= 0.0.10-1"
   },
-
-  test = {
-    dependencies = {
-      "luacov >= 0.15.0-1",
-    }
-  },
-
 }
 
 env.homepage = "https://github.com/treadwelllane/lua-" .. env.name
